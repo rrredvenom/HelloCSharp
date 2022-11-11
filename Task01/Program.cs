980d232b79bafa7d6bf99a8db8a44e5f04b4bf7a
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-11 20:33:29 +0300

    Задача решена

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+
+    Console.WriteLine($"Да, число {a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"Нет, число {a} не является квадратом числа {b}");
+}
