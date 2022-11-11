9a827cb8879de5e61bded02d375e53e9dc70897e
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-11 21:20:49 +0300

    Задача решена

---
+++ Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = - num;
+if (num >= 0)
+{
+while (count <= num)
+{
+    Console.Write(count + " ");
+    count ++;
+}
+}
+else
+{
+    while (count >= num)
+    {
+Console.Write(count + " ");
+    count ++;
+}
+}
