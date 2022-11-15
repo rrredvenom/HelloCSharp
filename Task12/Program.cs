2deae018d9ed5f8be2e16fd9f755044f37559671
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-15 11:53:15 +0300

    Добавлено альтернативное решение

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -21,3 +21,24 @@ else
 Console.WriteLine($"Остаток от деления {num1} на число {num2} = {num1 % num2}");
 }
 Multiple(a,b);
+
+// Console.WriteLine("Введите число 1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// bool Multiplicity (int number1, int number2)
+// {
+//      return number1 % number2 == 0;
+// }
+
+// int MultiplicitySecond (int number1, int number2)
+// { 
+//     return number1 % number2;
+// }
+
+// // bool result = Multiplicity(num1, num2);
+// // Console.WriteLine(result ? "Первое число кратно второму" : $"Некратно, остаток = {num1 % num2}" );
+
+// int result2 = MultiplicitySecond(num1, num2);
+// Console.WriteLine(result2 == 0 ? "Первое число кратно второму" : $"Некратно, остаток = {result2}" );
