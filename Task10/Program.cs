d61fcdbca6fd27266d3f425c5b991188557cc252
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-17 13:26:36 +0300

    Задача решена

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string showNumber(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        int second = (num % 100 - num % 10) / 10;
+        return ("Вторая цифра числа -> " + second);
+    }
+    else
+    {
+        return "Введенное число не трехзначное!";
+    }
+}
+Console.WriteLine(showNumber(num1));
