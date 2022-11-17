06b6d72c93e48c30374fdffb38f1c8ae476f6988
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-17 14:16:09 +0300

    Задача решена

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число от 1 до 7, соответствующее дню недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string dayWeek(int n)
+{
+    if (n > 5 && n < 8)
+    {
+    return ("Это выходной!");
+    }
+
+else if (n > 0 && n < 6 )
+{
+    return ("Это будний день!");
+}
+
+else 
+{
+    return ("Такого дня недели не существует!");
+}
+}
+Console.WriteLine(dayWeek(n));
