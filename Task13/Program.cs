72ce9cab3cf2d009ca0b9abb2af6fa23255e2bcc
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-17 13:39:20 +0300

    Задача решена

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string showNumber(int num)
+{
+    int i = 0;
+    int n = num;
+    if (n < 100)
+    {
+        return ("Третья цифра в числе отсутствует");
+    }
+    else
+    {
+        while (n > 999)
+        {
+            n = n / 10;
+            i++;
+        }
+
+        int third = n % 10;
+        return ("Третья цифра числа -> " + third);
+    }
+}
+Console.WriteLine(showNumber(num1));
