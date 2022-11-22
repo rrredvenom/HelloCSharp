feb1e7e791d2d2c03d3773264b3bca06b75ec131
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-22 11:20:02 +0300

    Задача решена

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumLength(int num)
+{
+    int n = 1;
+    for (int i = 0; i < 10; i++)
+    {
+        num = num / 10;
+        if(num!= 0) n ++;
+        else break;
+    }
+    return n;
+}
+int numLength = NumLength(num: number);
+Console.WriteLine(numLength);
