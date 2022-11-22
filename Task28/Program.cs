a88966c2fd260c6e448ec2724a98b190c2e2e5eb
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-22 12:15:33 +0300

    Задача решена

---
+++ Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int factorial(int number)
+{
+    if (number == 1)
+    {
+        return 1;
+    }
+    return number * factorial(number - 1);
+}
+
+Console.WriteLine(factorial(N));
