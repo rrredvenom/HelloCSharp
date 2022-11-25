fa9390168dc86707f231b58b0321cd7490e860d7
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-11-25 12:00:36 +0300

    Задача решена

---
+++ Task35/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int CountElements (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 9 && arr [i] < 100) count ++;
+    }
+    return count;
+}
+int[] array = CreateArrayRndInt(123, 0, 1000);
+PrintArray(array);
+
+int countElements = CountElements(array);
+if (countElements > 0) Console.WriteLine($"Количество двузначных элементов равно {countElements}");
+else Console.WriteLine("Двузеачных элементов нет");
