d86be3a9ff4bc0c4b9e2763b6c27b97047790dd3
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-12-06 10:29:29 +0300

    Задача решена

---
+++ Task55/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+        
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+            
+        }
+            Console.WriteLine("|");
+    }
+}
+
+int[,] SwapRowsColumns(int[,] matrix)
+{
+    int[,] temp = new int[matrix.GetLength(1), matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            temp[j, i] = matrix[i, j];
+        }
+    }
+    return temp;
+}
+
+void Output(int[,] matrix)
+{
+    if (matrix.GetLength(1) == matrix.GetLength(0))
+    {
+        int[,] swapedArray = SwapRowsColumns(matrix);
+        PrintMatrix(swapedArray);
+    }
+    else
+    {
+        Console.WriteLine("\nМассив не квадратный, процедура не выполнима.");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, -50, 50);
+PrintMatrix(array2D);
+Console.WriteLine();
+Output(array2D);
