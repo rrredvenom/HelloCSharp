ca9d5a83e62e87c0bc1ac271e84e1a6d80d9cafa
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-12-09 15:34:05 +0300

    Задача решена

---
+++ Task67/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int numb)
+{
+    if (numb != 0)
+
+        return numb % 10 + SumDigits(numb / 10);
+
+    return 0;
+}
+Console.WriteLine(SumDigits(Math.Abs(number)));
