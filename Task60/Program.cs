c001cafb0ac0ac6e3487797c5a12aa8ae56fa1ea
Ekaterina Egorova <vocamus.chaos@gmail.com>
2022-12-09 14:06:28 +0300

    Задача решена

---
+++ Task60/Program.cs
@@ -0,0 +1,41 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+void CreateMatrixRndInt(int [,,] matrix)
+{
+    int tmpry = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i,j,k] = tmpry; tmpry++;
+            }
+        }
+    }
+}
+void PrintMatrix(int [,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+Console.WriteLine("Задайте размер трехмерного массива: x, y, z : ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int [,,] numbers = new int[x,y,z];
+CreateMatrixRndInt(numbers);
+PrintMatrix(numbers);
